145d7f525396ee2712183b59222373dcf61f3e17
TerrenceBeckham <terrencebeckham@icloud.com>
2016-11-08 12:48:41 +0200

    Dropped and recreated DataBase

--- db/migrate/20161107160218_create_places.rb
+++ db/migrate/20161107160218_create_places.rb
@@ -1,11 +1,14 @@
 class CreatePlaces < ActiveRecord::Migration
   def change
     create_table :places do |t|
+      
       t.string :name
-      t.string :description
+      t.text :description
       t.string :address
       
       t.timestamps
     end
   end
 end
+
+
