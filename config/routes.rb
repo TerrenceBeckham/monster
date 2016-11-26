96818967d1b990f888ade7aa4bafe94fffa2b3f2
TerrenceBeckham <terrencebeckham@icloud.com>
2016-11-26 09:09:30 +0200

    Made the submit button save comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+  root 'places#index'
+  resources :places do
+    resources :comments, only: :create
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-root 'places#index'
 
-resources :places
+
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
