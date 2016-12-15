9747acedddabc37231c879b6812b79ccee5d465e
TerrenceBeckham <terrencebeckham@icloud.com>
2016-12-15 10:04:06 +0200

    Added notification Mailer

---
+++ app/mailers/notification_mailer.rb
@@ -0,0 +1,14 @@
+class NotificationMailer < ActionMailer::Base
+  default from: "no-reply@nomsterapp.com"
+
+  def comment_added
+    mail(to: "terrencebeckham@icloud.com", subject: "A comment has been added to your place")
+  end
+    
+end
+
+
+
+
+
+
