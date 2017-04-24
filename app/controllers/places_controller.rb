9a5d10b3c966113037be8fdf453b24bcec904a73
TerrenceBeckham <terrencebeckham@icloud.com>
2017-04-24 07:33:25 +0200

    Adjusted image uploade capability

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -13,11 +13,11 @@ before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]  #This
 
   def create
     @place = current_user.places.create(place_params)
-      if @place.valid? 
-        redirect_to root_path
-      else
-        render :new, status: :unprocessable_entity
-      end
+    if @place.valid? then
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
    end
 
 
--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -12,8 +12,8 @@ class PictureUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog   #gives major error if enabled on local host
+  #storage :file
+  storage :fog   #gives major error if enabled on local host
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
